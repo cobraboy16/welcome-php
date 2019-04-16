f1aaf994e2f04514faae98f32936bbaaca2414c6
cobraboy16 <33321872+cobraboy16@users.noreply.github.com>
2019-04-16 12:49:35 -0400

    Update index.php

--- index.php
+++ index.php
@@ -218,7 +218,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
    }
   
    if (empty($_POST["email"])) {
-     $emailErr = "Email is required";
+     $emailErr = "Email is required, give it to me";
    } else {
      $email = test_input($_POST["email"]);
      // check if e-mail address is well-formed
